45da0f09c7f92b797e84b046da0f12d7dbedd06a
Chris <chrisware707@gmail.com>
2020-09-15 23:01:26 -0700

    working on row and col

--- sudoku-project-master/sudoku.c
+++ sudoku-project-master/sudoku.c
@@ -2,12 +2,15 @@
 #include <unistd.h> 
 #include <stdlib.h>
 #include <getopt.h>
+#include <stdbool.h>
 
 /* These are the only two global variables allowed in your program */
 static int verbose = 0;
 static int use_fork = 0;
 
-
+struct Sudoku {
+    int array[9][9];
+};
 // This is a simple function to parse the --fork argument.
 // It also supports --verbose, -v
 void parse_args(int argc, char *argv[])
@@ -39,6 +42,10 @@ void parse_args(int argc, char *argv[])
     }
 }
 
+void row_Valid(int m, int n, int array[m][n]);
+void col_Valid(int m, int n, int array[m][n]);
+void subGrid_Valid(int array[9][9]);
+
 
 int main(int argc, char *argv[])
 {
@@ -52,22 +59,57 @@ int main(int argc, char *argv[])
 
     printf("Enter a sudoku solution\n");
     int test = 0;
-    int myArray[9][9];
+    struct Sudoku mySudoku;
 
+    //scan from a file using < and place solution in struct array
     for(int i = 0; i < 9; i++) {
         for (int j = 0 ; j < 9; j++) {
             scanf("%d",&test);
-            myArray[i][j] = test;
+            mySudoku.array[i][j] = test;
         }
     }
-
+    row_Valid(0, 0, &mySudoku.array);
+    col_Valid(0, 0, &mySudoku.array);
 
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            printf("%d ", myArray[i][j]);
+            printf("%d ", mySudoku.array[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
 
+void row_Valid(int m, int n, int array[m][n]) {
+    bool testArray[9] = {0,0,0,0,0,0,0,0,0};
+
+    for (int i = 0; i < 9; i++) {
+        int number = array[m][i];
+        if(array[m][i] < 1 || array[m][i] > 9 || testArray[(array[m][i])-1] == 1) {
+            printf("Row %d doesn't have the required values.\n", m + 1);
+            return;
+        }
+        else {
+            testArray[array[m][i]-1] = 1;
+        }
+    }
+}
+
+void col_Valid(int m, int n, int array[m][n]) {
+    bool testArray[9] = {0,0,0,0,0,0,0,0,0};
+
+    for (int i = 0; i < 9; i++) {
+        int number = array[i][n];
+        if (array[i][n] < 1 || array[i][n] > 9 || testArray[(array[i][n])-1] == 1) {
+            printf("Column %d doesn't have the required values.\n", n + 1);
+            return;
+        }
+        else {
+            testArray[array[i][n]-1] = 1;
+        }
+    }
+}
+void subGrid_Valid(int array[9][9]) {
+
+}
