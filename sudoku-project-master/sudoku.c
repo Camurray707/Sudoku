33447a3dd938f0524efc185ded99d00cf5fcd2f5
Chris <chrisware707@gmail.com>
2020-09-21 16:39:04 -0700

    simple threads done

--- sudoku-project-master/sudoku.c
+++ sudoku-project-master/sudoku.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <sys/types.h>
 
 /* These are the only two global variables allowed in your program */
 static int verbose = 0;
@@ -11,7 +13,12 @@ static int use_fork = 0;
 
 struct Sudoku {
     int array[9][9];
+    int row_start;
+    int col_start;
+    int sub_row;
+    int sub_col;
 };
+
 // This is a simple function to parse the --fork argument.
 // It also supports --verbose, -v
 void parse_args(int argc, char *argv[])
@@ -43,9 +50,9 @@ void parse_args(int argc, char *argv[])
     }
 }
 
-void row_Valid(int rowNumber, struct Sudoku sudoku);
-void col_Valid(int colNumber, struct Sudoku sudoku);
-void subGrid_Valid(int array[9][9], int row, int col);
+void *row_Valid(void* arg);
+void *col_Valid(void* arg);
+void *subGrid_Valid(void* arg);
 
 
 int main(int argc, char *argv[])
@@ -61,6 +68,10 @@ int main(int argc, char *argv[])
     printf("Enter a sudoku solution\n");
     int test = 0;
     struct Sudoku mySudoku;
+    mySudoku.row_start = 0;
+    mySudoku.col_start = 0;
+    mySudoku.sub_col = 0;
+    mySudoku.sub_row = 0;
 
     //scan from a file using < and place solution in struct array
     for(int i = 0; i < 9; i++) {
@@ -69,17 +80,8 @@ int main(int argc, char *argv[])
             mySudoku.array[i][j] = test;
         }
     }
-    for (int i = 0; i < 9; i++) {
-        row_Valid(i, mySudoku);
-        col_Valid(i, mySudoku);
-    }
-    
-    for (int start_row = 0; start_row < 9; start_row += 3) {
-        for (int start_col = 0; start_col < 9; start_col += 3) {
-            subGrid_Valid(mySudoku.array, start_row, start_col);
-        }
-    }
 
+    //print formatted sudoku array
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             printf("%d ", mySudoku.array[i][j]);
@@ -87,71 +89,102 @@ int main(int argc, char *argv[])
         printf("\n");
     }
 
+    //threads for row and column checking
+    for (int i = 0; i < 9; i++) {
+        pthread_t rowThread;
+        pthread_create(&rowThread, NULL, row_Valid, &mySudoku);
+        pthread_join(rowThread, NULL);
+
+        pthread_t colThread;
+        pthread_create(&colThread, NULL, col_Valid, &mySudoku);
+        pthread_join(colThread, NULL);
+    }
+
+    //threads for subgrid checking
+    for (int i = 0; i < 3; i++) {
+        pthread_t subGridThread;
+        pthread_create(&subGridThread, NULL, subGrid_Valid, &mySudoku);
+        pthread_join(subGridThread, NULL);
+    }
 
     return 0;
 }
 
 
-void row_Valid(int rowNumber, struct Sudoku sudoku) {
+void* row_Valid(void* arg) {
+    struct Sudoku *sudoku;
+    sudoku = (struct Sudoku *) arg;
     bool testArray[9] = {0,0,0,0,0,0,0,0,0};
 
     for (int i = 0; i < 9; i++) {
-        int number = sudoku.array[rowNumber][i];
-        if(sudoku.array[rowNumber][i] < 1 || sudoku.array[rowNumber][i] > 9 || 
-        testArray[(sudoku.array[rowNumber][i])-1] == 1) {
-            printf("Row %d doesn't have the required values.\n", rowNumber + 1);
-            return;
+        if(sudoku->array[sudoku->row_start][i] < 1 || sudoku->array[sudoku->row_start][i] > 9 ||
+        testArray[(sudoku->array[sudoku->row_start][i])-1] == 1) {
+            printf("Row %d doesn't have the required values.\n", sudoku->row_start + 1);
+            sudoku->row_start++;
+            return NULL;
         }
         else {
-            testArray[sudoku.array[rowNumber][i]-1] = 1;
+            testArray[sudoku->array[sudoku->row_start][i]-1] = 1;
         }
     }
+    sudoku->row_start++;
 }
 
-void col_Valid(int colNumber, struct Sudoku sudoku) {
+void* col_Valid(void* arg) {
+    struct Sudoku *sudoku;
+    sudoku = (struct Sudoku *) arg;
     bool testArray[9] = {0,0,0,0,0,0,0,0,0};
 
     for (int i = 0; i < 9; i++) {
-        if (sudoku.array[i][colNumber] < 1 || sudoku.array[i][colNumber] > 9 || 
-        testArray[(sudoku.array[i][colNumber])-1] == 1) {
-            printf("Column %d doesn't have the required values.\n", colNumber + 1);
-            return;
+        if (sudoku->array[i][sudoku->col_start] < 1 || sudoku->array[i][sudoku->col_start] > 9 ||
+        testArray[(sudoku->array[i][sudoku->col_start])-1] == 1) {
+            printf("Column %d doesn't have the required values.\n", sudoku->col_start + 1);
+            sudoku->col_start++;
+            return NULL;
         }
         else {
-            testArray[sudoku.array[i][colNumber]-1] = 1;
+            testArray[sudoku->array[i][sudoku->col_start]-1] = 1;
         }
     }
+    sudoku->col_start++;
+
 }
 
 
-void subGrid_Valid(int array[9][9], int row,int col){
+void* subGrid_Valid(void* arg){
+    struct Sudoku *sudoku;
+    sudoku = (struct Sudoku *) arg;
     bool testArray[9] = {0,0,0,0,0,0,0,0,0};
     
-    for (int r = row; r < row + 3; r ++) {
-        for (int c = col; c < col + 3; c++) {
-            if(array[r][c] < 1 || array[r][c] > 9 || testArray[(array[r][c])-1] == 1){
-                if(row <= 2)
+    for (int r = sudoku->sub_row; r < sudoku->sub_row + 3; r ++) {
+        for (int c = sudoku->sub_col; c < sudoku->sub_col + 3; c++) {
+            if(sudoku->array[r][c] < 1 || sudoku->array[r][c] > 9 || testArray[(sudoku->array[r][c])-1] == 1){
+                if(sudoku->sub_row <= 2)
                     printf("Top ");
-                else if(row<= 5)
+                else if(sudoku->sub_row<= 5)
                     printf("Middle ");
                 else
                     printf("Bottom ");
     
-                if(col <= 2)
+                if(sudoku->sub_col <= 2)
                     printf("left ");
-                else if(col <= 5)
+                else if(sudoku->sub_col <= 5)
                     printf("center ");
                 else
                     printf("right ");
 
                 printf("subgrid doesn't have the required values.\n");
-                return;
+                sudoku->sub_row+=3;
+                sudoku->sub_col+=3;
+                return NULL;
             }
             else {
-            testArray[array[r][c]-1] = 1;
+            testArray[sudoku->array[r][c]-1] = 1;
             }
         }
     }
+    sudoku->sub_row+=3;
+    sudoku->sub_col+=3;
 }
 
 
